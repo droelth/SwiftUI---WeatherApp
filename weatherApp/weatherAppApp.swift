aaea56e1f76740300fbccae8682338fc77495b61
Berkay <berkaysutlu94@gmail.com>
2023-09-18 11:42:24 +0300

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Berkay Sutlu on 18.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
